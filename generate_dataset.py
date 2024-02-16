1e93d31e16f7521a5a7557dfd1c55ccc9186e9eb
Antolin1 <joseantonio.hernandez6@um.es>
2024-02-16 16:48:38 +0100

    filtered test model and filter invalid queries

--- generate_dataset.py
+++ generate_dataset.py
@@ -7,8 +7,7 @@ import openai
 import pandas as pd
 from tqdm import tqdm
 
-from seed_metamodels.seed_sample_yakindu import SEED_METAMODEL, COMPLEX_PAIRS, SIMPLE_PAIRS
-from seed_metamodels.seed_sample_relational import SEED_METAMODEL_RELATIONAL, RELATIONAL_PAIRS
+from seed_metamodels.seed_sample_yakindu import SEED_METAMODEL, OR_PAIRS, NORMAL_PAIRS, FIND_PAIRS, TYPE_PAIRS
 from text2vql.postprocessor import postprocess_nl_queries
 from text2vql.template import get_formatted_nl_query, get_instruction_nl_queries
 
@@ -65,12 +64,12 @@ def get_metamodels(db, min_elements=30, max_elements=60):
 
 def main(args):
     df = get_metamodels(args.db, args.min_elements, args.max_elements)
-    df = df.sample(n=250, random_state=42)
+    df = df.sample(n=args.sample, random_state=42)
     for _, row in tqdm(df.iterrows(), desc='Generating dataset', total=df.shape[0]):
-        output_pairs = call_gpt(SEED_METAMODEL, COMPLEX_PAIRS, row['definition'], row['id'], requested_queries=5)
-        output_pairs += call_gpt(SEED_METAMODEL, SIMPLE_PAIRS, row['definition'], row['id'], requested_queries=5)
-        output_pairs += call_gpt(SEED_METAMODEL_RELATIONAL, RELATIONAL_PAIRS, row['definition'], row['id'],
-                                 requested_queries=5)
+        output_pairs = call_gpt(SEED_METAMODEL, OR_PAIRS, row['definition'], row['id'], requested_queries=10)
+        output_pairs += call_gpt(SEED_METAMODEL, NORMAL_PAIRS, row['definition'], row['id'], requested_queries=10)
+        output_pairs += call_gpt(SEED_METAMODEL, FIND_PAIRS, row['definition'], row['id'], requested_queries=10)
+        output_pairs += call_gpt(SEED_METAMODEL, TYPE_PAIRS, row['definition'], row['id'], requested_queries=10)
         save_to_db(args.db, row['id'], output_pairs)
 
 
@@ -78,8 +77,9 @@ if __name__ == '__main__':
     # parse arguments
     parser = argparse.ArgumentParser(description='Generate dataset by calling GPT')
     parser.add_argument('--db', type=str, default='dataset.db', help='database file')
-    parser.add_argument('--max_elements', type=int, default=50, help='max number of elements in metamodel')
+    parser.add_argument('--max_elements', type=int, default=100, help='max number of elements in metamodel')
     parser.add_argument('--min_elements', type=int, default=30, help='min number of elements in metamodel')
+    parser.add_argument('--sample', type=int, default=300, help='number of metamodels to sample')
 
     args = parser.parse_args()
     main(args)
--- seed_metamodels/seed_sample_yakindu.py
+++ seed_metamodels/seed_sample_yakindu.py
@@ -2,78 +2,98 @@ from text2vql.metamodel import MetaModel
 
 SEED_METAMODEL = MetaModel('seed_metamodels/yakindu_simplified.ecore')
 
-NL1 = "Transitions with their sources and targets"
-QUERY1 = """pattern transition(t : Transition, src : Vertex, trg : Vertex) {
-	Transition.source(t, src);
-	Transition.target(t, trg);
+OR_NL1 = "Vertices that are either entry or final state"
+OR_QUERY1 = """pattern entryOrFinalState(s : Vertex) {
+    Entry(s);
+} or {
+    FinalState(s);
 }"""
 
-NL2 = "All entries with their regions"
-QUERY2 = """pattern entryInRegion(r1 : Region, e1 : Entry) {
-	Region.vertices(r1, e1);
+OR_NL2 = "Retrieve all vertices that have incoming or outgoing transitions"
+OR_QUERY2 = """pattern incomingOrOutgoing(vertex: Vertex){
+    Vertex.incomingTransitions(vertex,_);
+} or {
+    Vertex.outgoingTransitions(vertex,_);
 }"""
 
-NL3 = "Regions that have several entries"
-QUERY3 = """pattern regionWithSeveralEntries(r1 : Region) {
-    Region.vertices(r1, e1);
-    Region.vertices(r1, e2);
-    e1 != e2;
+OR_NL3 = "State that are either entry state or has incoming transition"
+OR_QUERY3 = """pattern entryOrIncoming(vertex: Vertex){
+    Entry(vertex);
+} or {
+    Vertex.incomingTransitions(vertex,_);
 }"""
 
-NL4 = "All transitions that point to an entry"
-QUERY4 = """pattern transitionToEntry(t : Transition) {
+OR_NL4 = "Give me all transitions with the same source or same target"
+OR_QUERY4 = """pattern sameSourceOrTarget(transition1: Transition, transition2: Transition){
+    Transition.source(transition1,source);
+    Transition.source(transition2,source);
+} or {
+    Transition.target(transition1,target);
+    Transition.target(transition2,target);
+}"""
+
+OR_PAIRS = [(OR_QUERY1, OR_NL1), (OR_QUERY2, OR_NL2), (OR_QUERY3, OR_NL3), (OR_QUERY4, OR_NL4)]
+
+NORMAL_NL1 = "Transitions with their sources and targets"
+NORMAL_QUERY1 = """pattern transition(transition : Transition, source : Vertex, vertex : Vertex) {
+    Transition.source(transition, source);
+    Transition.target(transition, vertex);
+}"""
+
+NORMAL_NL2 = "All entries with their regions"
+NORMAL_QUERY2 = """pattern entryInRegion(region : Region, entry : Entry) {
+    Region.vertices(region, entry);
+}"""
+
+NORMAL_NL3 = "Regions that have several vertexes"
+NORMAL_QUERY3 = """pattern regionWithSeveralEntries(region : Region) {
+    Region.vertices(region, vertex1);
+    Region.vertices(region, vertex2);
+    vertex1 != vertex2;
+}"""
+
+NORMAL_NL4 = "Entries that have multiple outgoing transitions"
+NORMAL_QUERY4 = """pattern entryWithMultipleOutgoingTransitions(entry : Entry) {
+    Entry.outgoingTransitions(entry, transition1);
+    Entry.outgoingTransitions(entry, transition2);
+    transition1 != transition2;
+}"""
+
+NORMAL_PAIRS = [(NORMAL_QUERY1, NORMAL_NL1), (NORMAL_QUERY2, NORMAL_NL2), (NORMAL_QUERY3, NORMAL_NL3),
+                (NORMAL_QUERY4, NORMAL_NL4)]
+
+
+TYPE_NL1 = "RegularState is a vertex"
+TYPE_QUERY1 = """pattern regularState(state: Vertex){
+    RegularState(state);
+}"""
+
+TYPE_NL2 = "Entry is a Pseudostate"
+TYPE_QUERY2 = """pattern entry(entry: Pseudostate){
+    Entry(entry);
+}"""
+
+TYPE_PAIRS = [(TYPE_QUERY1, TYPE_NL1), (TYPE_QUERY2, TYPE_NL2)]
+
+FIND_NL1 = "Entries that do not have outgoing transitions"
+FIND_QUERY1 = """pattern entryWithoutOutgoingTransitions(e : Entry) {
     Entry(e);
-    Transition.target(t, e);
-}"""
-
-NL5 = "Entries that do not have outgoing transitions"
-QUERY5 = """pattern entryWithoutOutgoingTransitions(e : Entry) {
-    neg find transition(_, e, _);
+    neg find hasOutgoingTransition(e);
 }
 
 //auxiliary pattern
-pattern transition(t : Transition, src : Vertex, trg : Vertex) {
-	Transition.source(t, src);
-	Transition.target(t, trg);
+pattern hasOutgoingTransition(vertex : Vertex) {
+    Vertex.outgoingTransitions(vertex,_);
 }"""
 
-NL6 = "Entries that have multiple outgoing transitions"
-QUERY6 = """pattern entryWithMultipleOutgoingTransitions(e : Entry) {
-    Entry.outgoingTransitions(e, t1);
-	Entry.outgoingTransitions(e, t2);
-	t1!=t2;
-}"""
-
-NL7 = "All exit states with at least one outgoing transition"
-QUERY7 = """pattern exitStateWithOutgoingTransition(e : Exit) {
-    Exit.outgoingTransitions(e, _);
-}"""
-
-NL8 = "Regions that have no states"
-QUERY8 = """pattern noStateInRegion(region: Region) {
-	neg find stateInRegion(region, _);
+FIND_NL2 = "Regions that have no states"
+FIND_QUERY2 = """pattern noStateInRegion(region: Region) {
+    neg find StateInRegion(region, _);
 }
 
 //auxiliary pattern
 pattern stateInRegion(region: Region, state: State) {
-	Region.vertices(region, state);
+    Region.vertices(region, state);
 }"""
 
-NL9 = "States that are either entry or synchronization state"
-QUERY9 = """pattern entryOrSynchronizedState(s : Pseudostate) {
-    Entry(s);
-} or {
-    Synchronization(s);
-}"""
-
-NL10 = "Statecharts that have at least one region"
-QUERY10 = """pattern statechartWithRegion(sc : Statechart) {
-    Statechart.regions(sc, _);
-}"""
-
-PAIRS = [(QUERY3, NL3), (QUERY4, NL4),
-         (QUERY5, NL5), (QUERY6, NL6), (QUERY7, NL7), (QUERY8, NL8),
-         (QUERY9, NL9)]
-COMPLEX_PAIRS = [(QUERY9, NL9), (QUERY8, NL8), (QUERY5, NL5), (QUERY3, NL3), (QUERY6, NL6)]
-SIMPLE_PAIRS = [(QUERY4, NL4), (QUERY7, NL7), (QUERY1, NL1), (QUERY2, NL2), (QUERY10, NL10)]
-SEED_SAMPLE = (SEED_METAMODEL, PAIRS)
+FIND_PAIRS = [(FIND_QUERY1, FIND_NL1), (FIND_QUERY2, FIND_NL2)]
