98b6db33703b9958d3b14acbe3e10ec7add4147b
Antolin1 <joseantonio.hernandez6@um.es>
2024-05-29 12:44:50 +0200

    present the results

--- present_results.py
+++ present_results.py
@@ -9,20 +9,16 @@ def main(args):
     print(f'Pass@5:{len(results[results["has_correct"]]) / len(results)}')
     print(f'Correct:{len(results[results["has_correct"]])} out of {len(results)}')
 
-    if args.csv_outputs:
+    outputs = pd.read_csv(args.csv_results)
 
-        outputs = pd.read_csv(args.csv_outputs)
+    for c in set(outputs["construct"]):
+        m = outputs[outputs["construct"] == c]
+        print(f'Pass@5 {c}:{len(m[m["has_correct"]]) / len(m)}')
 
-        merged = pd.merge(results, outputs, on="id")
+    # train benchmark queries
+    merged_bench = outputs[outputs["train_benchmark"].notna()]
 
-        for c in set(merged["construct"]):
-            m = merged[merged["construct"] == c]
-            print(f'Pass@5 {c}:{len(m[m["has_correct"]]) / len(m)}')
-
-        # train benchmark queries
-        merged_bench = merged[merged["train_benchmark"].notna()]
-
-        print(f'Pass@5 TB:{len(merged_bench[merged_bench["has_correct"]]) / len(merged_bench)}')
+    print(f'Pass@5 TB:{len(merged_bench[merged_bench["has_correct"]]) / len(merged_bench)}')
 
 
 if __name__ == '__main__':
