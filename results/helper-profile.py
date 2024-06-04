3101245f225fa37fc1c82260b8e41f7c81afbaf2
Antolin1 <joseantonio.hernandez6@um.es>
2024-06-04 13:58:33 +0200

    updated results

--- results/helper-profile.py
+++ results/helper-profile.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-ai = pd.read_csv("profiles/full_dataset.csv").set_index('id')
+ai = pd.read_csv("profiles/final_dataset.csv").set_index('id')
 res = pd.read_csv("profiles/profiles_raw.csv").set_index('id')
 merded = ai.join(res)
 merded.to_csv("profiles/profiles.csv")
