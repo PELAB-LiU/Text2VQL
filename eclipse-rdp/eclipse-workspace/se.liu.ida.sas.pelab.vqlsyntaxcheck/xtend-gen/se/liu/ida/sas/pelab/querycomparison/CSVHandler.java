77499b6607f953eb8d24b82e6c01191331a4e273
Máté Földiák <mate.foldiak@liu.se>
2024-07-15 16:48:53 +0200

    Workspace cleanup

--- eclipse-rdp/eclipse-workspace/se.liu.ida.sas.pelab.vqlsyntaxcheck/xtend-gen/se/liu/ida/sas/pelab/querycomparison/CSVHandler.java
+++ eclipse-rdp/eclipse-workspace/se.liu.ida.sas.pelab.vqlsyntaxcheck/xtend-gen/se/liu/ida/sas/pelab/querycomparison/CSVHandler.java
@@ -1,13 +1,19 @@
 package se.liu.ida.sas.pelab.querycomparison;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
 public class CSVHandler {
   private final HashMap<Object, Object> record = CollectionLiterals.<Object, Object>newHashMap();
 
-  private final /* CSVPrinter */Object output;
+  private final CSVPrinter output;
 
   public static final String OUT_ID = "id";
 
@@ -19,46 +25,62 @@ public class CSVHandler {
 
   public static final String OUT_MATCH = "match_ok";
 
-  public static /* Iterable<CSVRecord> */Object parse(final String file) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nCSVRecord cannot be resolved to a type."
-      + "\nThe method or field CSVFormat is undefined"
-      + "\nRFC4180 cannot be resolved"
-      + "\nwithDelimiter cannot be resolved"
-      + "\nwithFirstRecordAsHeader cannot be resolved"
-      + "\nparse cannot be resolved");
+  public static Iterable<CSVRecord> parse(final String file) {
+    try {
+      CSVFormat _withFirstRecordAsHeader = CSVFormat.RFC4180.withDelimiter(',').withFirstRecordAsHeader();
+      FileReader _fileReader = new FileReader(file);
+      final Iterable<CSVRecord> records = _withFirstRecordAsHeader.parse(_fileReader);
+      return records;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 
   public CSVHandler(final String file) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method or field CSVFormat is undefined"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nRFC4180 cannot be resolved"
-      + "\nwithDelimiter cannot be resolved"
-      + "\nprint cannot be resolved"
-      + "\nprint cannot be resolved"
-      + "\nprintRecord cannot be resolved"
-      + "\nflush cannot be resolved");
+    try {
+      final CSVFormat format = CSVFormat.RFC4180.withDelimiter(',');
+      if ((file == null)) {
+        this.output = format.print(System.out);
+      } else {
+        FileWriter _fileWriter = new FileWriter(file);
+        this.output = format.print(_fileWriter);
+      }
+      this.output.printRecord(
+        CSVHandler.OUT_ID, 
+        CSVHandler.OUT_CORRECT, 
+        CSVHandler.OUT_BASEMATCHCOUNT, 
+        CSVHandler.OUT_SYNT, 
+        CSVHandler.OUT_MATCH);
+      this.output.flush();
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 
   public Object put(final String column, final Object value) {
     return this.record.put(column, value.toString());
   }
 
-  public Object commit() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nprintRecord cannot be resolved"
-      + "\nflush cannot be resolved");
+  public void commit() {
+    try {
+      this.output.printRecord(
+        this.record.get(CSVHandler.OUT_ID), 
+        this.record.get(CSVHandler.OUT_CORRECT), 
+        this.record.get(CSVHandler.OUT_BASEMATCHCOUNT), 
+        this.record.get(CSVHandler.OUT_SYNT), 
+        this.record.get(CSVHandler.OUT_MATCH));
+      this.record.clear();
+      this.output.flush();
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 
-  public Object close() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe field CSVHandler.output refers to the missing type CSVPrinter"
-      + "\nclose cannot be resolved");
+  public void close() {
+    try {
+      this.output.close(true);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
