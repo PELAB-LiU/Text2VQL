9161e97ed58b629cec5b3d93f11898495e13704d
Máté Földiák <mate.foldiak@liu.se>
2025-10-28 17:44:36 +0100

    Incr: Some evaluation and evaluation validation

--- dataset_construction/text2vql/evaluation/evaluate.py
+++ dataset_construction/text2vql/evaluation/evaluate.py
@@ -30,7 +30,7 @@ def getJavaMethodName(header_java):
 class Evaluator:
     def __init__(self, db, truth):
         self.db = db
-        self.timeout = 60*60*5
+        self.timeout = 60*60*5 # 5 hours?
         self.endpoint = "http://localhost:63028"
         self.truth = {}
 
@@ -45,23 +45,41 @@ class Evaluator:
                 if (row_id not in self.truth) and (row_id is not None) and (row_id.isdigit()):
                     self.truth[int(row_id)] = row
 
-
+    def extractQuery(self, string):
+        pattern = r"```(?:vql|java|ocl)([\s\S]*?)(?:```|$)"
+        match = re.search(pattern, string, re.IGNORECASE)
+        return match.group(1).strip() if match else None
 
     def getTest(self, caseid):
         with sqlite3.connect(self.db) as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT id, lang, query FROM evaluation WHERE caseid = ? AND shotid=1 AND finetune=1", (caseid,))
+            #cursor.execute("SELECT id, lang, query FROM evaluation WHERE caseid = ? AND shotid=1 AND finetune=1", (caseid,))
+            cursor.execute("SELECT id, lang, query FROM evaluation WHERE caseid = ? AND syntax IS NULL AND semantics IS NULL", (caseid,))
             return cursor.fetchall()
+        
     def getDomain(self, caseid):
         with sqlite3.connect(self.db) as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT DISTINCT domain FROM evaluation WHERE caseid = ?", (caseid,))
             return cursor.fetchone()[0]
+    
+    def update(self, ids, data):
+        with sqlite3.connect(self.db) as conn:
+            cursor = conn.cursor()
+            for id in ids:
+                entry = data[id]
+                cursor.execute("UPDATE evaluation SET syntax = ?, diagnostics = ?, semantics = ?, idicator = ? WHERE id = ?",
+                        (entry.syntax.syntax, str(entry.syntax.diagnostics), entry.semantics.semantics, entry.semantics.indicator, id))
+            conn.commit()
+        
 
     def process(self, caseid):
         domain = self.getDomain(caseid)
         allshots = self.getTest(caseid)
 
+        if not allshots:
+            return AttrDict({})
+        
         vqlname = getVQLQueryName(self.truth[caseid]['header_vql'])
         javaname = getJavaMethodName(self.truth[caseid]['header_java'])
 
@@ -69,12 +87,12 @@ class Evaluator:
             "wd": os.getcwd(),
             "metamodel": f"test_metamodel/{domain}.ecore",
             'jar': f"test_metamodel/{domain}.jar",
-            'models': '../results/testmodels',
+            'models': f'../results/testmodels/{domain}',
             'testcase': {
                 'truth': {'id': -1, 'entry': vqlname, 'query': self.truth[caseid]['truth_vql']} ,
-                'vql': [{'id': x[0], 'entry': vqlname, 'query':x[2]} for x in allshots if x[1]=='vql'],
-                'ocl': [{'id': x[0], 'entry': '', 'query':x[2]} for x in allshots if x[1]=='ocl'],
-                'java': [{'id': x[0], 'entry': javaname, 'query':x[2]} for x in allshots if x[1]=='java']
+                'vql': [{'id': x[0], 'entry': vqlname, 'query': self.extractQuery(x[2])} for x in allshots if x[1]=='vql'],
+                'ocl': [{'id': x[0], 'entry': '', 'query': self.extractQuery(x[2])} for x in allshots if x[1]=='ocl'],
+                'java': [{'id': x[0], 'entry': javaname, 'query': self.extractQuery(x[2])} for x in allshots if x[1]=='java']
             }
         }
         print('==========================================================')
@@ -91,9 +109,18 @@ class Evaluator:
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse dataset')
-    parser.add_argument('--db', type=str, default='../finetuning/evaluation-sample.db')
+    parser.add_argument('--db', type=str, default='../finetuning/evaluation(5).db')
     parser.add_argument('--truth', type=str, default='test_metamodel/truth.csv')
+    parser.add_argument('--id', type=int, default=None)
     args = parser.parse_args()
 
     eval = Evaluator(args.db, args.truth)
-    print(eval.process(2))
+    if args.id is not None:
+        result = eval.process(args.id)
+        eval.update(result.keys(), result)
+    else:
+        for i in range(0, 38+1):
+            if i==23:
+                continue
+            result = eval.process(i)
+            eval.update(result.keys(), result)
