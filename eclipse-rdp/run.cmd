46c627b9cac8f6fa4c51e6257df1bdfa26d5d4c7
Máté Földiák <mate.foldiak@liu.se>
2025-10-29 14:27:07 +0100

    Chore: Remove old rdp-based development environment. VSCode devcontainers are the new way.

--- eclipse-rdp/run.cmd
+++
@@ -1,9 +0,0 @@
-docker run -d ^
-  -v \\wsl$\Ubuntu\tmp\.X11-unix:/tmp/.X11-unix ^
-  -v \\wsl$\Ubuntu\mnt\wslg:/mnt/wslg ^
-  --name=text2vql ^
-  --security-opt seccomp=unconfined ^
-  -p 4050:3389 ^
-  --shm-size="1gb" ^
-  -v %cd%/../dataset_construction:/config/dataset_construction ^
-  text2vql
